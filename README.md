73d8fac6f12c76bdc62af7b1cb8a938bbeb98a9a
Dominik Lechl <dominik.lechl@alturos.com>
2024-11-18 12:39:58 +0100

    Setup which allows a GUI

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bruteforce
+
+## Setup
+
+Install [Conan](https://docs.conan.io/2/installation.html).
+
+When using CLion, install the [Conan plugin](https://www.jetbrains.com/help/clion/conan-plugin.html).
--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <wx/wx.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit(); // This method is called on application startup
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame()
+        : wxFrame(nullptr, wxID_ANY, "wxWidgets Test App", wxDefaultPosition, wxSize(400, 300)) {
+        // Add a simple button to the frame
+        new wxButton(this, wxID_OK, "Click Me", wxPoint(150, 100), wxSize(100, 50));
+    }
+};
+
+bool MyApp::OnInit() {
+    MyFrame* frame = new MyFrame();
+    frame->Show(true); // Show the frame
+    return true; // Start the event loop
 }
+
+wxIMPLEMENT_APP(MyApp);
