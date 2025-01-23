245d2b52f2111b8e0e5a5b86eb1ed2425af5d171
Dominik Lechl <dominik.lechl@alturos.com>
2025-01-23 13:27:24 +0100

    increase font sizes in main screen

--- main.cpp
+++ main.cpp
@@ -93,14 +93,19 @@ public:
         vbox->Add(divider, 0, wxEXPAND | wxALL, 20);
 
         auto* nameField = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(300, -1));
-        nameField->SetHint("Enter name");
+        nameField->SetFont(wxFont(16, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+        nameField->SetHint("Enter your name");
         vbox->Add(nameField, 0, wxALIGN_CENTER | wxTOP, 20);
 
         auto* startGameButton = new wxButton(this, wxID_ANY, "Start Game");
-        startGameButton->Enable(false); // Disable until a name is entered
-        vbox->Add(startGameButton, 0, wxALIGN_CENTER | wxTOP, 10);
+        startGameButton->SetFont(wxFont(14, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+        startGameButton->SetMinSize(wxSize(150, 40));
+        startGameButton->Enable(false);
+        vbox->Add(startGameButton, 0, wxALIGN_CENTER | wxTOP, 20);
 
         auto* highscoreButton = new wxButton(this, wxID_ANY, "Highscores");
+        highscoreButton->SetFont(wxFont(14, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+        highscoreButton->SetMinSize(wxSize(150, 40));
         vbox->Add(highscoreButton, 0, wxALIGN_CENTER | wxTOP, 10);
 
         nameField->Bind(wxEVT_TEXT, [startGameButton](wxCommandEvent& event) {
