97a9c0667031e690615ff2e93c74608ea083f63b
Dominik Lechl <dominik.lechl@alturos.com>
2025-01-23 14:45:34 +0100

    add game timer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <wx/wx.h>
 #include <wx/statline.h>
 #include <wx/dcbuffer.h>
+#include <wx/timer.h>
+
+const int GAME_DURATION_SECONDS = 300;
 
 class MainMenuPanel;
 class GamePanel;
@@ -134,7 +137,8 @@ private:
 class GamePanel : public wxPanel {
 public:
     GamePanel(wxWindow* parent, MainFrame* mainFrame)
-       : wxPanel(parent), mainFrame(mainFrame) {
+       : wxPanel(parent), mainFrame(mainFrame), remainingTime(GAME_DURATION_SECONDS) {
+
         auto* mainSizer = new wxBoxSizer(wxVERTICAL);
 
         auto* headerSizer = new wxBoxSizer(wxHORIZONTAL);
@@ -143,9 +147,20 @@ public:
         backButton->Bind(wxEVT_BUTTON, &GamePanel::OnBackToMenu, this);
         headerSizer->Add(backButton, 0, wxALIGN_LEFT | wxTOP | wxLEFT, 10);
 
+        auto* titleSizer = new wxBoxSizer(wxVERTICAL);
         auto* title = new wxStaticText(this, wxID_ANY, "Level 1", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER);
         title->SetFont(wxFont(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
-        headerSizer->Add(title, 1, wxALIGN_CENTER | wxALIGN_CENTER_HORIZONTAL | wxTOP | wxLEFT, 10);
+        titleSizer->Add(title, 0, wxALIGN_CENTER_HORIZONTAL | wxTOP, 10);
+
+        headerSizer->Add(titleSizer, 1, wxALIGN_CENTER_VERTICAL);
+
+        // Timer label
+        auto* timerSizer = new wxBoxSizer(wxVERTICAL);
+        timerLabel = new wxStaticText(this, wxID_ANY, "05:00", wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+        timerLabel->SetFont(wxFont(14, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+        timerSizer->Add(timerLabel, 0, wxALIGN_RIGHT | wxTOP | wxRIGHT, 10);
+
+        headerSizer->Add(timerSizer, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 10);
 
         mainSizer->Add(headerSizer, 0, wxEXPAND | wxALL, 10);
 
@@ -153,12 +168,45 @@ public:
         mainSizer->Add(divider, 0, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
 
         SetSizer(mainSizer);
+
+        Bind(wxEVT_SHOW, &GamePanel::OnShow, this);
+
+        timer.Bind(wxEVT_TIMER, &GamePanel::OnTimer, this);
+        timer.Start(1000); // Each second
+
+        UpdateTimerLabel();
     }
 private:
     MainFrame* mainFrame;
 
+    wxStaticText* timerLabel;
+    wxTimer timer;
+    int remainingTime;
+
     void OnBackToMenu(wxCommandEvent& event) {
-        mainFrame->ShowMenu();
+        if (int response = wxMessageBox("Are you sure you want to go back to the menu? Progress will be lost.", "Confirm", wxYES_NO | wxICON_QUESTION, this); response == wxYES) {
+            mainFrame->ShowMenu();
+        }
+    }
+
+    void OnTimer(wxTimerEvent& event) {
+        if (remainingTime > 0) {
+            remainingTime--;
+            UpdateTimerLabel();
+        } else {
+            timer.Stop();
+            wxMessageBox("Time's up!", "Notification", wxOK | wxICON_INFORMATION, this);
+        }
+    }
+
+    void UpdateTimerLabel() {
+        int minutes = remainingTime / 60;
+        int seconds = remainingTime % 60;
+        timerLabel->SetLabel(wxString::Format("%02d:%02d", minutes, seconds));
+    }
+
+    void OnShow(wxShowEvent& event) {
+        remainingTime = GAME_DURATION_SECONDS;
     }
 };
 
